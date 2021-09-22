01dbe3860a61c697f9cc96700950cb853b5c47fd
elatorre <eric.latorrecrespo@ed.ac.uk>
2021-09-22 13:46:03 +0100

    environment

---
+++ env/load_environment.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Create and activate conda environment from yml file
+echo "Installing and activating conda environment."
+
+eval "$(conda shell.bash hook)"
+conda env create --file env/stochastic_env.yml --name stochastic_env
